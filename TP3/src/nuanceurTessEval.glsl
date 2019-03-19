6e30c938f3d3e4faec006b08af2d7b92d21ce27b
Nathan Rullier <narul@l4712-05.info.polymtl.ca>
2019-03-19 09:42:11 -0400

    fix for tess

--- TP3/src/nuanceurTessEval.glsl
+++ TP3/src/nuanceurTessEval.glsl
@@ -77,6 +77,6 @@ void main()
     AttribsOut.L[2] = interpole( AttribsIn[0].L[2], AttribsIn[1].L[2], AttribsIn[2].L[2], AttribsIn[3].L[2] );
     AttribsOut.O = interpole( AttribsIn[0].O, AttribsIn[1].O, AttribsIn[2].O, AttribsIn[3].O );
     //AttribsOut.TexCoord = interpole( AttribsIn[0].TexCoord, AttribsIn[1].TexCoord, AttribsIn[2].TexCoord, AttribsIn[3].TexCoord );
-    //AttribsOut.TexCoord = AttribsOut.TexCoord.st;
+    //AttribsOut.TexCoord = AttribsOut.TexCoord.st; 
 
 }
